3727241f199b6368ede75285f0e3504eedd401f8
Anderson Freires <dre.son36@gmail.com>
2021-10-23 07:38:32 -0300

    Listando dados do banco

--- src/br/com/menu/dao/PedidoDao.java
+++ src/br/com/menu/dao/PedidoDao.java
@@ -3,6 +3,9 @@ package br.com.menu.dao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.menu.factory.ConnectionFactory;
 import br.com.menu.model.Pedido;
@@ -14,22 +17,22 @@ public class PedidoDao
 	
 	public void save(Pedido pedido) {
 		String sql="INSERT INTO pedido"
-		+ "(nome,quantidade,preco,tamanho,data) VALUES(?, ?, ?, ? ,?)";
+		+ "(nome,quantidade,preco,tamanho) VALUES(?, ?, ?, ? )";
 	Connection conn =null;
 	PreparedStatement pstm =null;
 	
 	
 	try {
 		conn = ConnectionFactory.createConnectSql();
-	    pstm= (PreparedStatement)  conn.prepareStatement(sql);
+	    pstm= conn.prepareStatement(sql);  
 	    pstm.setString(1,pedido.getNome());
 	    pstm.setInt(2,pedido.getQuantidade());
 	    pstm.setDouble(3,pedido.getPreco());
 	    pstm.setString(4,pedido.getTamanho());
-	    pstm.setDate(5, 
-	    		new Date(pedido.getDataCadastro().getTime()));
+	    //pstm.setDate(5, new Date(pedido.getDataCadastro().getTime()));
 	    
 	   pstm.execute();
+	   System.out.println("Pedido salvo com sucesso !");
 	}catch(Exception e) {
 		e.printStackTrace();
 	}finally {
@@ -48,4 +51,54 @@ public class PedidoDao
 		
 	}
 	}
+
+	public List<Pedido> getPedido(){
+		String sql= "SELECT * FROM  pedidos";
+		
+		List<Pedido> pedidos= new ArrayList<Pedido>();
+		
+		
+		Connection conn=null;
+		com.mysql.jdbc.PreparedStatement pstm=null;
+		//Classe que vai recuperar dados do banco
+		ResultSet rset=null;
+		
+		try {
+			conn=ConnectionFactory.createConnectSql();
+			pstm=(com.mysql.jdbc.PreparedStatement)conn.prepareStatement(sql);
+			rset=pstm.executeQuery();
+			
+			while(rset.next()) {
+			
+			Pedido pedido= new Pedido();
+			//recupera o id
+			pedido.setId(rset.getInt("id"));
+			//recupera o nome
+			pedido.setNome(rset.getString("nome"));
+			//recupera a quantidade
+			pedido.setQuantidade(rset.getInt("quantidade"));
+			//recupera o preco
+			pedido.setPreco(rset.getDouble("preco"));
+			
+			pedidos.add(pedido);
+			}
+					}catch (Exception e ) {
+						e.printStackTrace();
+					}finally {
+						try {
+						if(rset !=null) {
+							rset.close();
+						}
+						if(pstm !=null) {
+							pstm.close();
+						}
+						if(conn !=null) {
+							conn.close();
+						}
+					}catch (Exception e) {
+						e.printStackTrace();
+					}
+					}
+	 return pedidos;
+}
 }
